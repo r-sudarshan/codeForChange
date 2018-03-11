f702c4c36814fd9fd10a5c6dd46d2ef16ca70682
shelviadwihotama <shelvia.ho@gmail.com>
2018-03-11 17:59:19 +0800

    replaced auth-service with login-service and cleanup login codes for easier testing

--- app/src/pages/login/login.ts
+++ app/src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController, Platform, LoadingController } from 'ionic-angular';
 import { MenuPage } from '../menu/menu';
 import { LoginServiceProvider } from '../../providers/login-service/login-service';
+import { HomePage } from '../home/home';
 
 @IonicPage()
 @Component({
@@ -9,9 +10,10 @@ import { LoginServiceProvider } from '../../providers/login-service/login-servic
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  responseData: any;
   account: { username: string, password: string } = {
-    username: 'test@',
-    password: 'test'
+    username: "code4change",
+    password: "ChangeMe123!"
   };
 
   constructor(
@@ -26,7 +28,18 @@ export class LoginPage {
   }
 
   doLogin() {
-    this.navCtrl.setRoot(MenuPage);
+    this.loginService.login(this.account).then(result => 
+                {
+                  this.responseData = result;
+                  localStorage.setItem('userData', JSON.stringify(this.responseData));
+                  this.navCtrl.push(HomePage);
+                }, 
+              (err) => {
+                console.log("user not found!");
+                document.getElementById("LoginResult").innerHTML = "HttpResponse: " + JSON.stringify(this.responseData);
+                document.getElementById("LoginMessage").innerHTML = JSON.stringify("not working!");
+              }
+            );
   }
 
   dismiss() {
--- app/src/providers/login-service/login-service.ts
+++ app/src/providers/login-service/login-service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class LoginServiceProvider {
@@ -8,7 +9,15 @@ export class LoginServiceProvider {
     console.log('Hello LoginServiceProvider Provider');
   }
 
-  login(){
-  	
-  }
+  apiUrl = 'https://crm.beyond.org.sg/BSSTest';
+
+  login(account: any) {
+    return new Promise ((resolve, reject) => {
+      this.http.post(this.apiUrl + '/Ext.Sec1.Login.json',
+                     {SecretKey: 'TestSecretBSSChangeInProd', Username: account.username, Password: account.password, RememberMe: false}
+                    )
+                .subscribe(data => {resolve(data);}
+                           , err => {reject(err)})
+    })
+  };
 }
