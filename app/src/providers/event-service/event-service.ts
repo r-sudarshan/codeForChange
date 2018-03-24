3f6dc369904fcb5610f449ba5b02e1c111a3b93a
shelviadwihotama <shelvia.ho@gmail.com>
2018-03-24 19:38:29 +0800

    encoded json data to post request for events

--- app/src/providers/event-service/event-service.ts
+++ app/src/providers/event-service/event-service.ts
@@ -11,12 +11,29 @@ export class EventServiceProvider {
   }
 
   getEvents(){
+    var params = {
+      "SecretKey":"TestSecretBSSChangeInProd",
+      "EventName":"",
+      "EventType":"",
+      "GroupID":"",
+      "EventStatus":"",
+      "EventTags":"",
+      "OwnerGroup":"",
+      "RegStatus":"",
+      "StartDate":"1 Mar 2015",
+      "EndDate":"1 Mar 2018",
+      "CreatedBy":"",
+      "ShowEvent":"",
+      "ParentEvent":""
+    };
+
+    var encodedParams = encodeURIComponent(JSON.stringify(params));
     let head = new HttpHeaders({'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 'Accept': 'application/json, text/javascript, */*; q=0.01'});
-let r_options = {headers: head};
+    let r_options = {headers: head};
 
       return this.http.post(
         'https://crm.beyond.org.sg/BSSTest/Ext.Evt1.MainListing_Get.json', 
-        'data=%7B%22SecretKey%22%3A%22TestSecretBSSChangeInProd%22%2C%22EventName%22%3A%22%22%2C%22EventType%22%3A%22%22%2C%22GroupID%22%3A%22%22%2C%22EventStatus%22%3A%22%22%2C%22EventTags%22%3A%22%22%2C%22OwnerGroup%22%3A%22%22%2C%22RegStatus%22%3A%22%22%2C%22StartDate%22%3A%221+Mar+2015%22%2C%22EndDate%22%3A%221+Mar+2018%22%2C%22CreatedBy%22%3A%22%22%2C%22ShowEvent%22%3A%22%22%2C%22ParentEvent%22%3A%22%22%7D', 
+        'data=' + encodedParams,
         r_options)
       .map(response => response);
 /*
