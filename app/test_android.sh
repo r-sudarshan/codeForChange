4b149af52bafb22b5b258b2e354671f2e94b4bab
hari-allamraju <anarahari@gmail.com>
2018-05-05 14:29:37 +0800

    added script to test on android

---
+++ app/test_android.sh
@@ -0,0 +1,6 @@
+#point to the Java 8 or lower on your machine
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_172.jdk/Contents/Home
+#after you install Android Studio and the SDK, etc from that point this to the location of emulator;note we should not use emulator from the tools folder
+export PATH="/Users/hari/Library/Android/sdk/emulator":$JAVA_HOME:$PATH
+ionic cordova build android
+ionic cordova run android -c
