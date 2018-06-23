6f33c620aead7a0eea4e1d1f871b9b69dcfe4e12
shelviadwihotama <shelvia.ho@gmail.com>
2018-06-23 22:05:00 +0800

    enabled edit on profile + added save button

--- app/src/pages/profile/profile.ts
+++ app/src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, DateTime, FabContainer, FabButton } from 'ionic-angular';
 import { ProfileServiceProvider } from '../../providers/profile-service/profile-service';
 
 /**
@@ -41,7 +41,26 @@ export class ProfilePage {
     Email: "",
     FullAddress: ""
   }
+
+  isInputReadOnly: boolean;
+  isSaveDisabled: boolean;
+  
   constructor(public profileService: ProfileServiceProvider, public navCtrl: NavController, public navParams: NavParams) {
+    this.isInputReadOnly = true;
+    this.isSaveDisabled = true;
+  }
+
+  isReadOnly() {
+    return this.isInputReadOnly;
+  }
+
+  isSaveButtonDisabled() {
+    return this.isSaveDisabled;
+  }
+
+  enableEdit(fab: HTMLDivElement) {
+    this.isInputReadOnly = false;
+    fab.style.visibility = "hidden";
   }
 
   ionViewDidLoad() {
@@ -51,9 +70,13 @@ export class ProfilePage {
         this.userProfile.MiddleName = response["d"]["RetData"]["Tbl"]["Rows"]["0"]["MiddleName"];
         this.userProfile.LastName = response["d"]["RetData"]["Tbl"]["Rows"]["0"]["LastName"];
         this.userProfile.DisplayName = response["d"]["RetData"]["Tbl"]["Rows"]["0"]["DisplayName"];
-        this.userProfile.Gender = response["d"]["RetData"]["Tbl"]["Rows"]["0"]["Gender"];
-        // this.userProfile.Gender = "m";
-        this.userProfile.DateOfBirth = response["d"]["RetData"]["Tbl"]["Rows"]["0"]["BirthDate"];
+        // this.userProfile.Gender = response["d"]["RetData"]["Tbl"]["Rows"]["0"]["Gender"];
+        this.userProfile.Gender = "f";
+        // this.userProfile.DateOfBirth = response["d"]["RetData"]["Tbl"]["Rows"]["0"]["BirthDate"];
+        this.userProfile.DateOfBirth = new Date("01 Jan 2010").toDateString();
+        console.log("DATE: " + new Date("01 Jan 2010").toDateString);
+        console.log("DATE: " + new Date("01 Jan 2010").toLocaleString);
+        console.log("DATE: " + new Date("01 Jan 2010").toISOString);
         this.userProfile.Telephone = response["d"]["RetData"]["Tbl"]["Rows"]["0"]["Tel1"];
         this.userProfile.Mobile = response["d"]["RetData"]["Tbl"]["Rows"]["0"]["Mobile"];
         this.userProfile.Email = response["d"]["RetData"]["Tbl"]["Rows"]["0"]["Email1"];
@@ -63,7 +86,61 @@ export class ProfilePage {
         console.log("Error"+error.message);
       }
     )
-    console.log('ionViewDidLoad ProfilePage');
   }
 
+  onIonChange(fieldName, edit) {
+    switch(fieldName) {
+      case "FirstName":
+        if (this.userProfile.FirstName != edit.value) {
+          this.userProfile.FirstName = edit.value;
+          this.isSaveDisabled = false;
+        }
+        break;
+      case "MiddleName":
+        if (this.userProfile.MiddleName != edit.value) {
+          this.isSaveDisabled = false;
+        }
+        break;
+      case "LastName":
+        if (this.userProfile.LastName != edit.value) {
+          this.isSaveDisabled = false;
+        }
+        break;
+      case "DisplayName":
+        if (this.userProfile.DisplayName != edit.value) {
+          this.isSaveDisabled = false;
+        }
+        break;
+      case "Gender":
+        if (this.userProfile.Gender != edit) {
+          this.isSaveDisabled = false;
+        }
+        break;
+      case "DateOfBirth":
+        if (this.userProfile.DateOfBirth != edit.toString()) {
+          this.isSaveDisabled = true;
+        }
+        break;
+      case "Telephone":
+        if (this.userProfile.Telephone != edit.value) {
+          this.isSaveDisabled = false;
+        }
+        break;
+      case "Mobile":
+        if (this.userProfile.Mobile != edit.value) {
+          this.isSaveDisabled = false;
+        }
+        break;
+      case "Email":
+        if (this.userProfile.Email != edit.value) {
+          this.isSaveDisabled = false;
+        }
+        break;
+      case "FullAddress":
+        if (this.userProfile.FullAddress != edit.value) {
+          this.isSaveDisabled = false;
+        }
+        break;
+    }
+  }
 }
